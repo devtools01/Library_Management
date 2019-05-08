0d09697b2d617153a144b67b7e32cdadce60d69a
vishwasnm2 <vishwasm@devtools.in>
2019-05-08 18:14:07 +0530

    ITP-26 #comment removed

--- src/AdminSuccess.java
+++ src/AdminSuccess.java
@@ -34,9 +34,7 @@ public class AdminSuccess extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	
 	public AdminSuccess() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 371);
